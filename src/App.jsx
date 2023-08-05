df1eae9b6d2b9022a22278eee17d610b0c4ad95c
Mohit Krishna <mohitkrishna.s.s12@gmail.com>
2023-08-05 17:45:43 +0530

    add navbar using styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import { Navbar } from "./components/Navbar";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <BrowserRouter>
+     <Navbar />
+      <main className="sm:p-8 px-4 py-4 w-full bg-[#f9fafe] min-h-[calc(100vh-73px)]">
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,148 @@
+import React,{useState} from 'react'
+import { Link } from "react-router-dom";
+import logo from "../assets/logo.svg";
+import styled from "styled-components";
+import { IoHome, IoSettings } from "react-icons/io5";
+import { BsListUl } from "react-icons/bs";
+import { GiEscalator, GiHamburgerMenu } from "react-icons/gi";
+import { MdAccountCircle } from "react-icons/md";
+
+
+const StyledNavbar = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background-color: #fff;
+  padding-left: 1rem; /* 16px */
+  padding-right: 1rem; /* 16px */
+  padding-top: 0.25rem; /* 8px */
+  padding-bottom: 0.25rem; /* 8px */
+  border-bottom: 1px solid #e6ebf4; /* You can adjust this value as needed */
+  box-shadow: 0 4px 6px -1px rgb(0 0 0 / 0.1), 0 2px 4px -2px rgb(0 0 0 / 0.1);
+
+  /* Media query for small screens (sm breakpoint) */
+  @media screen and (max-width: 768px) {
+    padding-left: 2rem; /* 32px */
+    padding-right: 2rem; /* 32px */
+  }
+`;
+
+const NavItems = styled.div`
+  display: flex;
+  align-items: center;
+
+  @media screen and (max-width: 768px) {
+    display: none;
+    margin-top: 16px;
+  }
+`;
+
+const NavItem = styled(Link)`
+  display: flex;
+  align-items: center;
+  text-decoration: none; /* Remove the default underline on Link */
+  &:first-child {
+    margin-left: 0;
+  }
+  /* Rest of the styles */
+  margin-left: 30px;
+  cursor: pointer;
+  color: #5f6366;
+  font-weight: bold;
+
+  svg {
+    width: 20px;
+    height: 20px;
+    margin-right: 8px;
+  }
+`;
+
+const HamburgerMenu = styled.div`
+  display: none;
+
+  @media screen and (max-width: 768px) {
+    display: block;
+    cursor: pointer;
+  }
+`;
+
+const DropdownMenu = styled.div`
+  display: ${(props) => (props.showDropdown ? "flex" : "none")};
+  flex-direction: column;
+  background-color: #fff;
+  position: absolute;
+  top: 56px;
+  right: 16px;
+  padding: 8px;
+  border-radius: 4px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+  min-width: 150px;
+  z-index: 1;
+
+  a {
+    color: #333;
+    text-decoration: none;
+    padding: 8px;
+    border-bottom: 1px solid #f1f1f1;
+
+    &:last-child {
+      border-bottom: none;
+    }
+
+    &:hover {
+      background-color: #f1f1f1;
+    }
+  }
+  @media screen and (min-width: 768px) {
+    display: none;
+  }
+`;
+
+export const Navbar = () => {
+     const [showDropdown, setShowDropdown] = useState(false);
+
+     const handleDropdownToggle = () => {
+       setShowDropdown(!showDropdown);
+     };
+  return (
+      <StyledNavbar>
+        <Link to="/">
+          <img src={logo} alt="logo" className="logo-img" />
+        </Link>
+        <NavItems>
+          <NavItem to="/">
+            <IoHome />
+            Home
+          </NavItem>
+          <NavItem>
+            <BsListUl />
+            List
+          </NavItem>
+          <NavItem>
+            <GiEscalator />
+            Escalation
+          </NavItem>
+          <NavItem>
+            <IoSettings />
+            Settings
+          </NavItem>
+          <NavItem>
+            <MdAccountCircle />
+            Account
+          </NavItem>
+        </NavItems>
+        <HamburgerMenu onClick={handleDropdownToggle}>
+          <GiHamburgerMenu />
+        </HamburgerMenu>
+        {showDropdown && (
+          <DropdownMenu showDropdown={showDropdown}>
+            <Link to="/">Home</Link>
+            <Link>List</Link>
+            <Link>Escalation</Link>
+            <Link>Settings</Link>
+            <Link>Account</Link>
+          </DropdownMenu>
+        )}
+      </StyledNavbar>
+  );
+}
